dc71e1fc8109f821d823b927bf8e3b52b26c12da
Nicolas Durpoix <nicolas.durpoix82@gmail.com>
2020-11-21 20:04:13 +0100

    fix fs module file not found

--- functions/getFile.js
+++ functions/getFile.js
@@ -3,7 +3,12 @@ const formattedResponse = require("./utils/formattedResponse");
 
 exports.handler = async event => {
   try {
-    const json = await JSON.parse(fs.readFileSync("test.json", {encoding: "utf-8"}));
+    const json = await JSON.parse(
+      fs.readFileSync(
+        "https://trusting-curran-5d818b.netlify.app/test.json",
+        { encoding: "utf-8" }
+      )
+    );
     return formattedResponse(200, json);
   } catch (err) {
     console.log(err);
